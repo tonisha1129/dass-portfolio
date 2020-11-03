a5817a06987f21d9640fa2d4803f8ba5b97b58af
tonisha1129 <70670752+tonisha1129@users.noreply.github.com>
2020-11-03 18:04:45 -0500

    DEBUG  CHALLENGE COMPLETE

--- debug/sketch.js
+++ debug/sketch.js
@@ -3,23 +3,23 @@
 var faceSize = "one hundred" * 3;
 
 function setup() {
-    createcanvas(640, 480);
-    backGround(50);
-    noStoke();
+    createCanvas(640, 480);
+    background(50);
+    noStroke();
     fill("lightblue");
     ellipse(350, 320, faceSize); // face
 
-    fill(0 0 0);
-    ellipse 450, 250, 100; // right eye
-    ellipse(x, y, 100); / left eye
-    ellipse(x, y + 100, 100 50); // mouth
+    fill(0,0,0);
+    ellipse (450, 250, 100); // right eye
+    ellipse(250, 250, 100); // left eye
+    ellipse(350, 400, 100, 50); // mouth
 
-    fill(white);
+    fill("white");
     ellipse(150, 100, 200, 150);
     triangle(200, 200, 150, 175, 175, 150);
 
     fill("black");
     textSize(28);
-    textFont(monospace);
-    text("Nice work!" 65, 110);
+    textFont("monoSpace");
+    text("Nice work!", 65, 110);
 }
